cb6994e03708b1d90971bc610dc6b2c2153f284c
Viet <vietjul@gmail.com>
2022-02-19 13:19:28 -0500

    feat: finish IllegalOperationException

--- ams/IllegalOperationException.java
+++ ams/IllegalOperationException.java
@@ -2,25 +2,23 @@ package ams;
 import java.util.*;
 
 /**
- * On cancelling or completing reservation if it cannot be finalized, throw unchecked user defined exception
+ * On cancelling or completing reservation if it cannot be finalized, throw 
+ * unchecked user defined exception
  */
 public class IllegalOperationException extends RuntimeException {
 
-	/**
-	 * Default constructor
-	 */
-	public IllegalOperationException() {
-	}
-
-	/**
-	 * 
-	 */
 	private String accountNumber;
+	private String reservationNumber;
 
 	/**
-	 * 
+	 * @param accountNumber 
+	 * @param reservationNumber
 	 */
-	private String reservationNumber;
+	public void IllegalOperationException(String accountNumber, String reservationNumber) {
+		Helpers.validateParameters(accountNumber, reservationNumber);
+		this.accountNumber = accountNumber;
+		this.reservationNumber = reservationNumber;
+	}
 
 	/**
 	 * Reasons for completing reservation errors (e.g. lodge doesn't have available rooms at the time of booking).
@@ -28,21 +26,10 @@ public class IllegalOperationException extends RuntimeException {
 	private String message;
 
 	/**
-	 * return "Error cancelling reservation: " + reservationNumber + " for account: " + accountNumber + " . Reservation can only be cancelled if the start date is in the future.";
-	 * return "Error completing reservation: " + reservationNumber + " for account: " + accountNumber + " . Reason: " + message;
-	 * @return
+	 * @return Error message string
 	 */
 	public String toString() {
-		// TODO implement here
-		return "";
-	}
-
-	/**
-	 * @param accountNumber 
-	 * @param reservationNumber
-	 */
-	public void IllegalOperationException(String accountNumber, String reservationNumber) {
-		// TODO implement here
+		return "Error modifying reservation: " + reservationNumber + " for account: " + accountNumber + ". Reason: " + message;
 	}
 
 }
